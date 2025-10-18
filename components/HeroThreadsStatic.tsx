173fe9fa88567a929a6126bfb599672d2ee84997
LuckysHorizon <fruitylucky@gmail.com>
2025-10-18 20:30:55 +0530

    Final Production Push

--- components/HeroThreadsStatic.tsx
+++ components/HeroThreadsStatic.tsx
@@ -1,19 +1,20 @@
-import React from "react"
-import styles from "./HeroThreadsStatic.module.css"
+import React from "react";
+import styles from "./HeroThreadsStatic.module.css";
 
 const HeroThreadsStatic = () => {
   const Token = () => (
     <span className={styles.token}>
-      <span className={styles.textReel}>REEL</span><span className={styles.textWhite}>HAUS</span>
+      <span className={styles.textReel}>REEL</span>
+      <span className={styles.textWhite}>HAUS</span>
     </span>
-  )
+  );
 
   const renderRow = () => (
-    <span>{Array.from({ length: 12 }).map((_, i) => <Token key={i} />)}</span>
-  )
+    <span>{Array.from({ length: 10 }).map((_, i) => <Token key={i} />)}</span>
+  );
 
   return (
-    <div id="threads-bg" className={`${styles.heroBackground}`} aria-hidden>
+    <div id="threads-bg" className={styles.heroBackground} aria-hidden="true">
       <div className={`${styles.ribbon} ${styles.ribbonWhite}`}>
         <div className={styles.inner}>{renderRow()}{renderRow()}</div>
       </div>
@@ -24,12 +25,13 @@ const HeroThreadsStatic = () => {
         <div className={styles.inner}>{renderRow()}{renderRow()}</div>
       </div>
       <div className={`${styles.ribbon} ${styles.ribbonBlack}`}>
+        <div className={styles.inner}>{renderRow()}{renderRow()}</div>
+      </div>
+      <div className={`${styles.ribbon} ${styles.ribbonWhite}`}>
         <div className={styles.inner}>{renderRow()}{renderRow()}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroThreadsStatic
-
-
+export default HeroThreadsStatic;
